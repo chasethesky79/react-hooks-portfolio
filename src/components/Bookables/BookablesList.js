fd56c67298df61d08bc78751297a07354580d583
Bharath Seshadri <bharathseshadri@kukubara7.local>
2021-12-25 20:22:21 -0600

    Fixed alignment of main page to vertically align children

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -3,26 +3,33 @@ import { useState } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
 export default function BookablesList() {
-    const group = 'Rooms';
+    const groups = bookables.reduce((acc, element) => {
+      if (!acc.includes(element.group)) {
+          acc = [...acc, element.group]
+      }
+      return acc;
+    }, []);
+    const [group, setGroup] = useState("Kit");
     const bookablesInGroup = bookables.filter(bookable => bookable.group === group);
-    const [bookableIndex, setBookableIndex] = useState(1);
+    const [bookableIndex, setBookableIndex] = useState(0);
 
     function changeBookable(selectedIndex) {
         setBookableIndex(selectedIndex);
     }
 
     return (
-        <>
-        <ul className='bookables items-list-nav'>
-            { bookablesInGroup.map((b, i) => <li key={b.id} className={i === bookableIndex ? 'selected': null }>
-                <button className='btn' onClick={() => changeBookable(i)}>{b.title}</button>
-            </li>)}
-        </ul>
-        <p>
-          <button className='btn' onClick={() => setBookableIndex(bookableIndex => (bookableIndex + 1) % bookablesInGroup.length)} autoFocus>
-              <FaArrowRight/><span>Next</span></button>
-        </p>
-        </>
+        <div>
+            <select value={group} onChange={(e) => setGroup(e.target.value)}>{groups.map(g => <option value={g} key={g}>{g}</option>)}</select>
+            <ul className='bookables items-list-nav'>
+                { bookablesInGroup.map((b, i) => <li key={b.id} className={i === bookableIndex ? 'selected': null }>
+                    <button className='btn' onClick={() => changeBookable(i)}>{b.title}</button>
+                </li>)}
+            </ul>
+            <p>
+            <button className='btn' onClick={() => setBookableIndex(bookableIndex => (bookableIndex + 1) % bookablesInGroup.length)} autoFocus>
+                <FaArrowRight/><span>Next</span></button>
+            </p>
+        </div>
     )
 
 }
