2af2573b2599d94c86f5cef3dd6070e548bb0e56
Bharath Seshadri <bharathseshadri@kukubara7.local>
2022-01-01 15:37:34 -0600

    More implementation of week picker

--- src/components/Week/WeekPicker.js
+++ src/components/Week/WeekPicker.js
@@ -5,7 +5,6 @@ import { FaArrowLeft, FaArrowRight, FaCalendarDay } from 'react-icons/fa';
 
 export default function WeekPicker({ date }) {
     const [week, dispatch ] = useReducer(weekPickerReducer, date, getWeek);
-    const { firstDayOfWeek, lastDayOfWeek } = week;
     return (
         <>
             <div className='week-picker'>
@@ -13,7 +12,7 @@ export default function WeekPicker({ date }) {
                 <button className='btn' onClick={ () => dispatch({ type: 'CURRENT_WEEK' })}  autoFocus><FaCalendarDay/><span>Today</span></button>
                 <button className='btn' onClick={ () => dispatch({ type: 'NEXT_WEEK' })}  autoFocus><FaArrowRight/><span>Next</span></button>
             </div>
-            <p style={{ textAlign: 'center' }}>{firstDayOfWeek.toDateString()} - {lastDayOfWeek.toDateString()}</p>
+            <p style={{ textAlign: 'center' }}>{week?.firstDayOfWeek?.toDateString()} - {week?.lastDayOfWeek?.toDateString()}</p>
         </>
     )
 }
