e11e082c77df3fee1e1e7bd1d07b2ce5f3c857cc
bharath.seshadri <bharath.seshadri@wheelsup.com>
2022-03-21 18:01:05 -0700

    Implemented userpicker with async/await

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+
 export default function UserPicker() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function fetchUsers() {
+            const usersJSON = await fetch('http://localhost:3001/users');
+            const users = await usersJSON.json();
+            setUsers(users);
+          }
+        fetchUsers();
+    }, [])
+
+    if (!users || users.length === 0) {
+        return <div>Empty Users</div>
+    }
+
     return (
-        <select>
-            <option>Users</option>
-        </select>
+       <select>
+           LENGTH {users?.length}
+           { users && users.map(({ name, id } ) => <option key={id}>{name}</option>)}
+       </select>
     )
 }
